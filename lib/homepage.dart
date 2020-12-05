e7811e47a6bb56ce1e6d074047cacc85008e87e2
gohar <gohardhuddi@gmail.com>
2020-12-05 14:25:51 -0800

    Adding 2 buttons simple and hard lavel

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
+  @override
+  _homepageState createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  child: Text(
+                    "Simple Mode",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                RaisedButton(
+                  child: Text(
+                    "Hard Mode",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 23.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
